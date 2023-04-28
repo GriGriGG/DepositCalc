20cadae0344e13da07596e7c9b781d5e2a1b6587
Grigorii Riabokon <le1taon1@gmail.com>
2023-04-28 22:05:21 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+money = int(input("Вводите сумму, которую планируете положить под проценты: "))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+bank1 = int((per_cent['ТКБ']) * (money/100))
+bank2 = int((per_cent['СКБ']) * (money/100))
+bank3 = int((per_cent['ВТБ']) * (money/100))
+bank4 = int((per_cent['СБЕР']) * (money/100))
+deposit = [bank1, bank2, bank3, bank4]
+print("За год вклада в каждом из банков = ",deposit)
+print("Максимальная сумма, которую вы можете заработать: ", max(deposit))
